164a95640b374f8e9194d5e07d4d55e8c956801f
Anderson Eloi Vaz <anderson.vaz@rikai.com.br>
2020-05-22 07:20:31 -0300

    Fore Is not null

---
+++ SalesWebMvc/Migrations/20200522094950_DepartmentForeinKey.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class DepartmentForeinKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SalesRecord_Seller_SelleriD",
+                table: "SalesRecord");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller");
+
+            migrationBuilder.RenameColumn(
+                name: "iD",
+                table: "Seller",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "SelleriD",
+                table: "SalesRecord",
+                newName: "SellerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SalesRecord_SelleriD",
+                table: "SalesRecord",
+                newName: "IX_SalesRecord_SellerId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Seller",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SalesRecord_Seller_SellerId",
+                table: "SalesRecord",
+                column: "SellerId",
+                principalTable: "Seller",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SalesRecord_Seller_SellerId",
+                table: "SalesRecord");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Seller",
+                newName: "iD");
+
+            migrationBuilder.RenameColumn(
+                name: "SellerId",
+                table: "SalesRecord",
+                newName: "SelleriD");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SalesRecord_SellerId",
+                table: "SalesRecord",
+                newName: "IX_SalesRecord_SelleriD");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Seller",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SalesRecord_Seller_SelleriD",
+                table: "SalesRecord",
+                column: "SelleriD",
+                principalTable: "Seller",
+                principalColumn: "iD",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -42,6 +42,7 @@ namespace SalesWebMvc
                     builder.MigrationsAssembly("SalesWebMvc")));
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerServices>();
+            services.AddScoped<DepartmentService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
